20c12b13f7c1fec939a65214029d431694394051
Balder <baldersandstrom.business@gmail.com>
2021-01-20 21:37:00 +0100

    Added documentation to get pub points

--- lib/override_text_scale_factor.dart
+++ lib/override_text_scale_factor.dart
@@ -1,3 +1,5 @@
+/// This package provides the [OverrideTextScaleFactor] widget
+/// to override text scale factor in parts of the widget tree
 library override_text_scale_factor;
 
 export 'src/widget.dart';
--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+/// Flutter widget that overrides [textScaleFactor] passed through [MediaQuery].
 class OverrideTextScaleFactor extends StatelessWidget {
-  final Widget child;
-  final double textScaleFactor;
-
+  /// Creates a [OverrideTextScaleFactor] widget
   const OverrideTextScaleFactor({
     @required this.child,
     this.textScaleFactor = 1.0,
   })  : assert(child != null),
         assert(textScaleFactor != null);
+
+  /// Widget to be used as child
+  ///
+  /// This cannot be null since there would be no
+  /// reason to use [OverrideTextScaleFactor] widget then
+  final Widget child;
+
+  /// The [textScaleFactor] used to override the systems' font scale
+  ///
+  /// By default this is set to 1.0
+  final double textScaleFactor;
 
   @override
   Widget build(BuildContext context) {
